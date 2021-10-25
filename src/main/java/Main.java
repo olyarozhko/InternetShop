e4166a395d5471cb9eda8a8b5db54c58938c6567
Korn'Dan <karret4444@gmail.com>
2021-10-25 15:50:16 +0300

    LoginMenu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,4 +4,6 @@ public class Main {
     public static void main(String[] args) {
         new LoginMenu().show();
     }
+    LoginMenu loginMenu = new LoginMenu();
+
 }
--- src/main/java/view/impl/LoginMenu.java
+++ src/main/java/view/impl/LoginMenu.java
@@ -16,7 +16,7 @@ public class LoginMenu implements Menu {
     @Override
     public void show() {
         showItems(items);
-        System.out.println("0. Exit");
+       /* System.out.println("0. Exit"); */
 
         scanner = new Scanner(System.in);
 
@@ -28,11 +28,11 @@ public class LoginMenu implements Menu {
                     loginSubMenu(scanner);
                     break;
                 case 2:
-                    loginSubMenu(scanner);
+                    registerSubMenu(scanner);
                     break;
-                case 0:
+                /*case 0:
                     exit();
-                    break;
+                    break;*/
             }
         }
     }
@@ -43,11 +43,29 @@ public class LoginMenu implements Menu {
     }
 
     private void loginSubMenu(Scanner scanner) {
+        scanner = new Scanner(System.in);
         System.out.println("input login:");
         String login = scanner.nextLine();
 
         System.out.println("input password:");
         String password = scanner.nextLine();
+        System.out.println("Are u sure?: \n");
+        System.out.println("login: " + login + "\npassword: " + password + "\n");
+        System.out.println("1.Yes, continue.\n2.No, back.\n");
+        int yesNo = scanner.nextInt();
+        switch (yesNo) {
+            case 1:
+                if (userService.login(login, password)) {
+                    new ProductMenu().show();
+                } else {
+                    System.out.println("Wrong username/password");
+                    show();
+                }
+                break;
+            case 2:
+                new LoginMenu().show();
+                break;
+        }
 
         if (userService.login(login, password)) {
             new ProductMenu().show();
@@ -58,6 +76,29 @@ public class LoginMenu implements Menu {
     }
 
     private void registerSubMenu(Scanner scanner) {
-        show(); //todo add impl
+        scanner = new Scanner(System.in);
+        System.out.println("input login:");
+        String login = scanner.nextLine();
+
+        System.out.println("input password:");
+        String password = scanner.nextLine();
+
+        System.out.println("Are u sure?: \n");
+        System.out.println("login: " + login + "\npassword: " + password + "\n");
+        System.out.println("1.Yes, continue.\n2.No, back.\n");
+        int yesNo = scanner.nextInt();
+        switch (yesNo) {
+            case 1:
+                System.out.println("Account created!");
+                new LoginMenu().show();
+                break;
+            case 2:
+                System.out.println("Account not created!");
+                new LoginMenu().show();
+                break;
+
+
+            // show(); //todo add impl
+        }
     }
 }
