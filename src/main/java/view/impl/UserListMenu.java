3b9bce42fd6b0c127500f2a3097a4bb5dd350fd2
orozhko <olyarozhko@gmail.com>
2021-10-27 22:58:52 +0300

    Admin Menu. UserListMenu class is added

---
+++ src/main/java/view/impl/UserListMenu.java
@@ -0,0 +1,20 @@
+package view.impl;
+
+import view.Menu;
+
+public class UserListMenu implements Menu {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void exit() {
+
+    }
+
+    @Override
+    public void showItems(String[] items) {
+        Menu.super.showItems(items);
+    }
+}
