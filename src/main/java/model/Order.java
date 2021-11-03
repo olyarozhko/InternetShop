a8a09b768e00d9525c21cc1a94c60e1c042a59d8
orozhko <olyarozhko@gmail.com>
2021-11-03 19:32:10 +0200

    Order model

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,14 +1,91 @@
 package model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Order {
-    private Integer number; //унікальне поле
+    private Integer id = 0; //унікальне поле
+    private static Integer idNext = 0;
     private String orderOwner; //хто замовив
     private Status state;
+    private List<Product> productList;
+    private Double sum;
 
-
-    enum Status {
+    public enum Status {
         NEW,
-        CONFIRMED
+        CONFIRMED,
+        CANCELLED
     }
 
+
+    public Order(String orderOwner, List<Product> productList) {
+        this.orderOwner = orderOwner;
+        this.productList = productList;
+        this.id = idNext ++;
+        this.state = Status.NEW;
+        this.sum = productList.stream().mapToDouble(Product::getPrice).sum();
+    }
+
+    public Order() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public static Integer getIdNext() {
+        return idNext;
+    }
+
+    public String getOrderOwner() {
+        return orderOwner;
+    }
+
+    public void setOrderOwner(String orderOwner) {
+        this.orderOwner = orderOwner;
+    }
+
+    public Status getState() {
+        return state;
+    }
+
+    public void setState(Status state) {
+        this.state = state;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public Double getSum() {
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        return '{' +
+                "id=" + id +
+                ", orderOwner='" + orderOwner + '\'' +
+                ", state=" + state +
+                ", productList=" + productList +
+                ", SUM=" + sum +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order order)) return false;
+        return getId().equals(order.getId()) && getOrderOwner().equals(order.getOrderOwner()) &&
+                getState() == order.getState() && getProductList().equals(order.getProductList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getOrderOwner(), getState(), getProductList());
+    }
 }
