dac64ee8b9236970070d6af6bc987b1c6a881c6a
orozhko <olyarozhko@gmail.com>
2021-11-04 18:37:05 +0200

    Product Servie Logic.added verification for not null Value of all the parameters

--- src/main/java/service/ProductServiceLogic.java
+++ src/main/java/service/ProductServiceLogic.java
@@ -24,22 +24,21 @@ public class ProductServiceLogic implements ProductService {
             }
         }
 
-        productDao.save(productNew);
+        productDao.addProduct(productNew);
         return new Response<>(productNew, true, null);
     }
 
     @Override
     public Response<Product> editProduct(Integer productToUpdateId, String nameNew, String firm, Integer price,
                                          Integer maxLoad, String loadType) {
-        var productUpdated = new Product(nameNew, firm, price, maxLoad, loadType);
         var productDao = new ProductDaoImpl();
-        var oldProduct = new ProductDaoImpl().getById(productToUpdateId);
+        var product = new ProductDaoImpl().getById(productToUpdateId);
 
         if (productToUpdateId == null) {
             return new Response<>(null, false, "\nProduct ID can\t be blank");
         }
 
-        if (oldProduct == null) {
+        if (product == null) {
             return new Response<>(null, false, "\nProduct with such ID doesn't exist.");
         }
 
@@ -47,13 +46,37 @@ public class ProductServiceLogic implements ProductService {
             return new Response<>(null, false, "\nProduct with such name already exists.");
         }
 
-        if (!loadType.equalsIgnoreCase("vertical")) {
-            if (!loadType.equalsIgnoreCase("frontal")) {
-                return new Response<>(null, false, "\nLoad type should be: 'Frontal' or 'Vertical'");
+        if(loadType != null) {
+            if (!loadType.equalsIgnoreCase("vertical")) {
+                if (!loadType.equalsIgnoreCase("frontal")) {
+                    return new Response<>(null, false, "\nLoad type should be: 'Frontal' or 'Vertical'");
+                }
             }
         }
-        productDao.update(oldProduct, productUpdated);
-        return new Response<>(productUpdated, true, null);
+        product.setLoadType(loadType);
+
+        if(nameNew != null){
+            product.setProductName(nameNew);
+        }
+
+        if(firm != null){
+            product.setFirm(firm);
+        }
+
+        if(price != null){
+            product.setPrice(price);
+        }
+
+        if(maxLoad != null){
+            product.setMaxLoad(maxLoad);
+        }
+
+        if(loadType != null){
+            product.setFirm(firm);
+        }
+
+        productDao.editProduct(productToUpdateId, product);
+        return new Response<>(product, true, null);
     }
 
 }
