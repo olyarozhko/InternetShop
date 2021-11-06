c30fed54a6b150a36af4a66667b19cd5fdb5e126
orozhko <olyarozhko@gmail.com>
2021-11-06 12:21:47 +0200

    Order and Product dao

--- src/main/java/dao/OrderDaoImpl.java
+++ src/main/java/dao/OrderDaoImpl.java
@@ -1,37 +1,123 @@
 package dao;
 
-import model.Order;
+import model.User;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrderDaoImpl implements OrderDao{
 
-    static List<Order> orderList = new ArrayList<>();
+public class UserDaoImpl implements UserDao {
+
+    private static List<User> userdb = new ArrayList<>();
 
     @Override
-    public void save(Order order) {
-        orderList.add(order);
+    public void readFromCsv(String filename) {
+        Path pathToFile = Paths.get(filename);
 
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(",");
+                User user = createUserForReadingFromCsv(attributes);
+                addUser(user);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private User createUserForReadingFromCsv(String[] metadata) {
+
+        String username = metadata[1];
+        String password = metadata[2];
+        boolean isLocked = Boolean.parseBoolean(metadata[3]);
+
+        return new User(username, password, isLocked);
     }
 
     @Override
-    public void changeOrderStatus(Integer orderId, Order.Status status) {
+    public void save(List<User> userdb) {
+        File userdbFile = new File("resources/userdbFile.csv");
 
+        if (!userdbFile.exists()) {
+            try {
+                File directory = new File(userdbFile.getParent());
+                if (!directory.exists()) {
+                    directory.mkdir();
+                }
+                userdbFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error occurred while creating directory or file");
+            }
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("resources/userdbFile.csv"), StandardCharsets.UTF_8))) {
+            for (User user : userdb) {
+                String CSV_SEPARATOR = ";";
+                String oneLine = (user.getUserId() < 0 ? "" : user.getUserId()) +
+                        CSV_SEPARATOR +
+                        (user.getUsername().trim().length() == 0 ? "" : user.getUsername()) +
+                        CSV_SEPARATOR +
+                        (user.getPassword().trim().length() == 0 ? "" : user.getPassword()) +
+                        CSV_SEPARATOR +
+                        (user.isLocked() ? "Yes" : "No");
+                bw.write(oneLine);
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            System.out.println("Error occurred while saving user");
+        }
     }
 
     @Override
-    public List<Order> getByUser(String username) {
+    public void addUser(User userNew) {
+
+        userdb.add(userNew);
+        save(userdb);
+    }
+
+    @Override
+    public void lockUser(Integer userId, boolean isLocked) {
+
+        for (User user : userdb) {
+            if (user.getUserId().equals(userId)) {
+                user.setLocked(isLocked);
+                save(userdb);
+            }
+        }
+    }
+
+    @Override
+    public User getByName(String username) {
+        for (User user : userdb) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
         return null;
     }
 
     @Override
-    public Order getById(Integer id) {
-        return null;
+    public List<User> getAll() {
+        return (userdb);
     }
 
     @Override
-    public List<Order> getAllOrders() {
-        return orderList;
+    public User getById(Integer userId) {
+
+        for (User user : userdb) {
+            if (user.getUserId().equals(userId)) {
+                return user;
+            }
+        }
+        return null;
     }
 }
--- src/main/java/dao/ProductDaoImpl.java
+++ src/main/java/dao/ProductDaoImpl.java
@@ -2,56 +2,127 @@ package dao;
 
 import model.Product;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductDaoImpl implements ProductDao{
-    static List<Product> productList = new ArrayList<>();
+public class ProductDaoImpl implements ProductDao {
 
+    private static List<Product> productdb = new ArrayList<>();
 
     @Override
-    public void addProduct(Product product) {
-        productList.add(product);
+    public void readFromCsv(String filename) {
+        Path pathToFile = Paths.get(filename);
 
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] attributes = line.split(",");
+                Product product = createProductForReadingFromCsv(attributes);
+                addProduct(product);
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Product createProductForReadingFromCsv(String[] metadata) {
+
+        String productName = metadata[1];
+        String firm = metadata[2];
+        Integer price = Integer.parseInt(metadata[3]);
+        Integer maxLoad = Integer.parseInt(metadata[4]);
+        String loadType = metadata[5];
+
+
+        return new Product(productName, firm, price,maxLoad,loadType);
     }
 
     @Override
-    public void update(Integer productId, Product productNew) {
+    public void addProduct(Product productNew) {
 
+        productdb.add(productNew);
+        save(productdb);
     }
 
     @Override
     public void save(List<Product> productdb) {
+        File productdbFile = new File("resources/productdbFile.csv");
 
+        if (!productdbFile.exists()) {
+            try {
+                File directory = new File(productdbFile.getParent());
+                if (!directory.exists()) {
+                    directory.mkdir();
+                }
+                productdbFile.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Error occurred while creating directory or file");
+            }
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("resources/productdbFile.csv"), StandardCharsets.UTF_8))) {
+            for (Product product : productdb) {
+                String CSV_SEPARATOR = ";";
+                String oneLine = (product.getProductId() < 0 ? "" : product.getProductId()) +
+                        CSV_SEPARATOR +
+                        (product.getProductName().trim().length() == 0 ? "" : product.getProductName()) +
+                        CSV_SEPARATOR +
+                        (product.getFirm().trim().length() == 0 ? "" : product.getFirm()) +
+                        CSV_SEPARATOR +
+                        (product.getPrice() < 0 ? "" : product.getPrice()) +
+                        CSV_SEPARATOR +
+                        (product.getMaxLoad() < 0 ? "" : product.getMaxLoad()) +
+                        CSV_SEPARATOR +
+                        (product.getLoadType());
+                bw.write(oneLine);
+                bw.newLine();
+            }
+            bw.flush();
+        } catch (IOException e) {
+            System.out.println("Error occurred while saving user");
+        }
     }
 
     @Override
     public Product getByName(String productName) {
-        return null;
-    }
-
-    @Override
-    public Product getById(Integer id) {
-        for (Product product : productList) {
-            if (product.getProductId().equals(id)) {
+        for (Product product : productdb) {
+            if (product.getProductName().equals(productName)) {
                 return product;
             }
         }
         return null;
     }
 
-    public List<Product> search(String searchString){
-        List<Product> searchProductsList = null;
-        for (Product product : productList) {
-            if (product.getProductName().contains(searchString)) {
-                searchProductsList.add(product);
+    @Override
+    public Product getById(Integer productId) {
+        for (Product product : productdb) {
+            if (product.getProductId().equals(productId)) {
+                return product;
             }
         }
-        return searchProductsList;
+        return null;
     }
 
-
+    @Override
     public List<Product> getAll() {
-        return productList;
+        return (productdb);
+    }
+
+    @Override
+    public void editProduct(Integer productId, Product productNew) {
+        for (Product product : productdb) {
+            if (product.getProductId().equals(productId)) {
+                int index = productdb.indexOf(product);
+                productdb.set(index,productNew);
+                save(productdb);
+            }
+        }
     }
 }
